7de59c97b0e9c4b6d90bc053635e47dd2db6b2c0
Anatoli Makarevich <makaroni4@gmail.com>
2017-03-17 21:30:01 +0100

    Focus query editor once opened

--- app/assets/javascripts/app/toggle_editor.js
+++ app/assets/javascripts/app/toggle_editor.js
@@ -2,12 +2,17 @@ $(function() {
   var $link = $(".js-toggle-query-form");
   var $queryFormContainer = $(".query-form-container");
   var $closeIcon = $(".js-close-query-form");
+  var editor = ace.edit("query-editor");
 
   $link.on("click", function(e) {
     e.preventDefault();
 
     $queryFormContainer.toggle();
     $link.toggle();
+
+    if($queryFormContainer.is(":visible")) {
+      editor.focus();
+    }
   });
 
   $closeIcon.on("click", function(e) {
