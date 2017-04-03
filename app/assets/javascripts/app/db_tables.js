8e0fceb8059c2875e7af1052b976176a8a7fe9ed
Anatoli Makarevich <makaroni4@gmail.com>
2017-04-04 00:22:53 +0200

    Added refresh button to db tables

--- app/assets/javascripts/app/db_tables.js
+++ app/assets/javascripts/app/db_tables.js
@@ -1,6 +1,14 @@
 $(function() {
-  $(".js-db-tables").dataTable({
-    paginate: false,
-    info: false
+  var $tables = $(".js-db-tables");
+
+  $tables.on("draw.dt", function() {
+    var $refreshButton = $(".js-refresh-db-tables");
+    $refreshButton.appendTo($(".js-tables-actions"));
+    $refreshButton.removeClass("hidden");
+  });
+
+  $tables.dataTable({
+    dom: "<'db-tables-controls'<'js-tables-actions'><f>>" +
+         "<'row'<'col-sm-12'tr>>"
   });
 });
